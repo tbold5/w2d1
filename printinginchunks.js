e18d1bab7d3691b5318c10a9e673c7b2b4a71dcc
Tuguldur <tuguldurbold5@gmail.com>
2018-10-01 19:00:07 +0000

    first commit, printing chunks with line breaks

---
+++ printinginchunks.js
@@ -0,0 +1,19 @@
+// require https because it is module
+var https = require('https');
+function getAndPrintHTMLChunks() {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+    https.get(requestOptions, function(response){
+        response.setEncoding('utf8');
+
+        response.on('data', function (data){
+            console.log(data + '\n');
+        /* Add your code here */
+        });
+    });
+}; 
+
+getAndPrintHTMLChunks();
